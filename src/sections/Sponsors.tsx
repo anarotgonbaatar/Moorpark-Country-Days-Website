e3b2a41c9c0acd95665517491095868ae6ab172e
anarotgo <anarotgo@yahoo.com>
2025-09-27 19:30:24 -0700

    Fixed folder name

--- src/sections/Sponsors.tsx
+++ src/sections/Sponsors.tsx
@@ -36,15 +36,15 @@ export default function Sponsors() {
 	const goldSponsors = [
 		{
 			sponsor_name: 'Adventist Health',
-			logo_url: 'public/Gold Sponsors/adventist-health.jpg',
+			logo_url: 'public/gold-sponsors/adventist-health.jpg',
 		},
 		{
 			sponsor_name: 'Clean Concepts',
-			logo_url: 'public/Gold Sponsors/clean-concepts.jpg',
+			logo_url: 'public/gold-sponsors/clean-concepts.jpg',
 		},
 		{
 			sponsor_name: 'MoorPark Karate Krav Maga',
-			logo_url: 'public/Gold Sponsors/karate-krav-maga.jpg',
+			logo_url: 'public/gold-sponsors/karate-krav-maga.jpg',
 		},
 	]
 	const silverSponsors = [
